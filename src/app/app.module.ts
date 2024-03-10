27c81869c9e107c5ab4b524f849883ede7bfc928
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2024-03-10 18:06:01 +0100

    header: add hardcoded header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { JournalEntryComponent } from './journal-entry/journal-entry.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, JournalEntryComponent],
+  declarations: [AppComponent, HomeComponent, JournalEntryComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
