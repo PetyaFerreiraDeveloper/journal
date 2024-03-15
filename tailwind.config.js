bcbeb4f10a7e16de050e324f818c5ef2b7c166bf
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2024-03-15 12:16:56 +0100

    tailwind-config- add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ module.exports = {
         main: ["Montserrat", "sans-serif"],
         alternative: ["Montserrat Alternates", "sans-serif"],
       },
+      colors: {
+        "dark-green": "#006400",
+        "darker-green": "#005000",
+        "darkest-green": "#004000",
+      },
+      backgroundImage: {
+        heroMain:
+          "linear-gradient(180deg, rgba(4,0,148,1) 0%, rgba(128,219,225,1) 100%)",
+        heroGreen:
+          "linear-gradient(180deg, rgba(102,140,48,1) 0%, rgba(210,221,196,1) 100%)",
+      },
     },
   },
   plugins: [],
