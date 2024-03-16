3ac8b54209e463d83079715b481982100513d258
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2024-03-16 23:48:43 +0100

    update create-entry component

--- src/app/journal/create-entry/create-entry.component.ts
+++ src/app/journal/create-entry/create-entry.component.ts
@@ -1,10 +1,40 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
+import { JournalService } from 'src/app/services/journal.service';
 
 @Component({
   selector: 'app-create-entry',
   templateUrl: './create-entry.component.html',
-  styleUrls: ['./create-entry.component.css']
+  styleUrls: ['./create-entry.component.css'],
 })
 export class CreateEntryComponent {
+  constructor(
+    private journalService: JournalService,
+    private location: Location
+  ) {}
 
+  backHandler(event: Event): void {
+    event.preventDefault();
+    this.location.back();
+  }
+
+  createHandler(
+    event: Event,
+    titleInput: string,
+    categoryInput: string,
+    blogChecked: boolean,
+    journalEntry: string
+  ): void {
+    event.preventDefault();
+
+    const entry = {
+      title: titleInput,
+      category: categoryInput,
+      journalEntry: journalEntry,
+      blog: blogChecked,
+    };
+    console.log(entry);
+
+    this.journalService.create$(entry).subscribe((data) => console.log(data));
+  }
 }
