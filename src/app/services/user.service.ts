69921c693a098be95a65fb16f3b374739975c5b4
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2024-03-16 17:32:26 +0100

    Save user to localstorage

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,28 +2,51 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
+import { User } from '../types/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   apiUrl = environment.apiUrl;
-  loginUrl = 'http://localhost:3030/users/login';
-  isLogged: boolean = true;
-  constructor(private httpClient: HttpClient) {}
+  authUrl = environment.authUrl;
+  // isLogged: boolean = true;
+  USER_KEY = '[user]';
+  user: User | undefined;
 
-  login(): void {
-    this.isLogged = true;
-  }
-
-  logout(): void {
-    this.isLogged = false;
-  }
-  loginValues = {
+  loginUser = {
+    firstName: 'Peter',
+    lastName: 'Petrov',
     email: 'peter@abv.bg',
     password: '123456',
   };
+
+  get isLogged(): boolean {
+    return !!this.user;
+  }
+
+  constructor(private httpClient: HttpClient) {
+    try {
+      // get user from local storage if there is one
+      const localStorageUser = localStorage.getItem(this.USER_KEY) || '';
+      this.user = JSON.parse(localStorageUser);
+    } catch (error) {
+      this.user = undefined;
+    }
+  }
+
+  logout(): void {
+    this.user = undefined;
+    // delete the user from local storage
+    localStorage.removeItem(this.USER_KEY);
+  }
+
   login$(): Observable<any> {
-    return this.httpClient.post<any>(this.loginUrl, this.loginValues);
+    this.user = this.loginUser;
+
+    // save the user in local storage
+    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+
+    return this.httpClient.post<any>(`${this.authUrl}/login`, this.loginUser);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
   apiUrl: 'http://localhost:3030/data/journals',
+  authUrl: 'http://localhost:3030/users',
 };
