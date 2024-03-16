0fdc940a2a0ee9fa5ddf4bfeb9bc6a0ec25e54f0
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2024-03-16 15:48:52 +0100

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ErrorComponent } from './error/error.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'about-us', component: AboutComponent },
+  {
+    path: '**',
+    component: ErrorComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/journal/journal-routing.module.ts
+++ src/app/journal/journal-routing.module.ts
@@ -3,15 +3,36 @@ import { Route, RouterModule } from '@angular/router';
 import { JournalListComponent } from './journal-list/journal-list.component';
 import { BlogListComponent } from './blog-list/blog-list.component';
 import { CreateEntryComponent } from './create-entry/create-entry.component';
+import { DetailsComponent } from './details/details.component';
 
 const routes: Route[] = [
   {
     path: 'my-journal',
-    component: JournalListComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: JournalListComponent,
+      },
+      {
+        path: ':journalId',
+        component: DetailsComponent,
+      },
+    ],
   },
   {
     path: 'blog',
-    component: BlogListComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: BlogListComponent,
+      },
+      {
+        path: ':blogId',
+        component: DetailsComponent,
+      },
+    ],
   },
   { path: 'create-entry', component: CreateEntryComponent },
 ];
