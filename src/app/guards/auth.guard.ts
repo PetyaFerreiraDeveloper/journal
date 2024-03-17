44fa5c9c6a678ab1fbad379a8e69b37f3e54a073
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2024-03-17 11:35:08 +0100

    AuthGuard-refacture code

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanActivateChild, Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate, CanActivateChild {
+  constructor(private userService: UserService, private router: Router) {}
+  canActivate(): boolean {
+    return this.checkAuth();
+  }
+
+  canActivateChild(): boolean {
+    return this.checkAuth();
+  }
+
+  private checkAuth(): boolean {
+    if (this.userService.isLogged) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+}
