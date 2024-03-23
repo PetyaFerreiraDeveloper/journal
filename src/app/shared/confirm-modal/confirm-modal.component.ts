8952641767066bc1a3aa9278a3604da4d13f636c
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2024-03-23 11:32:44 +0100

    create confirm modal component

---
+++ src/app/shared/confirm-modal/confirm-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-modal',
+  templateUrl: './confirm-modal.component.html',
+  styleUrls: ['./confirm-modal.component.css'],
+})
+export class ConfirmModalComponent {
+  @Input() title: string = '';
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,7 @@ import { EntryLabelComponent } from './entry-label/entry-label.component';
 import { NoEntriesComponent } from './no-entries/no-entries.component';
 import { EmailDirective } from './validators/email.directive';
 import { SlicePipe } from './pipes/slice.pipe';
+import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { SlicePipe } from './pipes/slice.pipe';
     NoEntriesComponent,
     EmailDirective,
     SlicePipe,
+    ConfirmModalComponent,
   ],
   imports: [CommonModule],
   exports: [
@@ -21,6 +23,7 @@ import { SlicePipe } from './pipes/slice.pipe';
     NoEntriesComponent,
     EmailDirective,
     SlicePipe,
+    ConfirmModalComponent
   ],
 })
 export class SharedModule {}
