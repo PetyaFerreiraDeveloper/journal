eee457d59f1ce7614f288a5be0923abc3245f54b
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2024-03-29 17:59:15 +0100

    MobileNav-close the navbar on link click

--- src/app/core/mobile-nav/mobile-nav.component.ts
+++ src/app/core/mobile-nav/mobile-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -7,11 +7,16 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./mobile-nav.component.css'],
 })
 export class MobileNavComponent {
+  @Output() linkClicked: EventEmitter<void> = new EventEmitter<void>();
   constructor(private userService: UserService) {}
   get isLogged(): boolean {
     return this.userService.isLogged;
   }
   get email(): string {
     return this.userService.user?.email || '';
+  }
+
+  onLinkClick(): void {
+    this.linkClicked.emit();
   }
 }
