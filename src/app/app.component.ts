306356f9254254d4c7b7cbc0d36a770980570e5a
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2024-03-29 17:59:42 +0100

    add code for handling clicks outside of navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'journal';
+  mobileNav = false;
+
+  toggleMobileNav(): void {
+    this.mobileNav = !this.mobileNav;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    const targetElement = event.target as HTMLElement;
+    if (
+      !targetElement.closest('app-mobile-nav') &&
+      !targetElement.closest('header')
+    ) {
+      console.log('click');
+      console.log('Mobile Nav Status:', this.mobileNav);
+      this.mobileNav = false;
+      console.log('Mobile Nav Status After Closing:', this.mobileNav);
+    }
+  }
 }
